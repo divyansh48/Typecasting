37e6e023c6d0ac8dae24848b790febf5e1cbee89
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-13 11:33:49 +0530

    Add files via upload
    all the basic concepts of typecasting in java is given in this java file.

---
+++ typecasting.java
@@ -0,0 +1,29 @@
+package com.divyansh;
+
+public class typecasting {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("tycasting");
+
+        int num = 25;
+        System.out.println((float)(num));
+
+        float fl = 34.45f;
+        int num1 = (int)(fl);
+        System.out.println(num1);
+
+        System.out.println("automatic type promotion in expression");
+
+        byte b = 23;
+        int  a = 21;
+        float c = 76.89f;
+        short d = 5373;
+        double e = 65.37838938;
+        double  result = (b+a) + (a/c) - (d*e) ;
+        System.out.println(result);
+
+
+    }
+}
